d99ba0849a4ed5573351333a93d57611a0d6e8f9
shariyerShazan <shariyershazan1@gmail.com>
2025-12-04 12:56:16 +0600

    getUser

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -1,1 +1,51 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+	"github.com/shariyerShazan/GoLang-Crud-MongoDB/models"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type UserController struct{
+	session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController{
+return &UserController{s}
+}
+
+func (uc UserController) GetUser (w http.ResponseWriter, r *http.Request, p httprouter.Params){
+	id := p.ByName("id")
+
+	if !bson.IsObjectIdHex(id){
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	oid := bson.ObjectIdHex(id)
+
+	u := models.User{}
+
+  if err := uc.session.DB("mongo-golang").C("users").FindId(oid).One(&u); err != nil{
+	w.WriteHeader(404)
+	return
+   }
+
+   uj, err :=json.Marshal(u)
+   if err!= nil{
+	   fmt.Println(err)
+   }
+
+w.Header().Set("Content-Type", "application/json")
+w.WriteHeader(http.StatusOK)
+fmt.Fprintf(w, "%s\n", uj)
+
+}
+
+
+
+
+
