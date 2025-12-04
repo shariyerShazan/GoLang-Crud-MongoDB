7d6fa8800290a96dbe5dd83ad3cc5e910526cff3
shariyerShazan <shariyershazan1@gmail.com>
2025-12-04 12:56:33 +0600

    createUser

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -46,6 +46,24 @@ fmt.Fprintf(w, "%s\n", uj)
 }
 
 
+func (uc UserController) CreateUser (w http.ResponseWriter, r *http.Request, _ httprouter.Params){
+	u := models.User{}
 
+	json.NewDecoder(r.Body).Decode(&u)
+
+	u.ID = bson.NewObjectId()
+
+	uc.session.DB("mongo-golang").C("users").Insert(u)
+
+	uj, err := json.Marshal(u)
+
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s\n", uj)
+}
 
 
